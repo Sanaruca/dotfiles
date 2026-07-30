3aa9cd33d47305d46e97d6df54c723cfcc0b75d2
sanaruca <sanaruca@gmail.com>
2026-07-30 01:49:22 -0400

    Translate keymaps comments to English

--- dot/config/nvim/lua/config/keymaps.lua
+++ dot/config/nvim/lua/config/keymaps.lua
@@ -2,18 +2,18 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Ctrl+j/k/l e i para navegar ventanas (ijkl layout: j=izq, i=arriba, k=abajo, l=der).
--- NOTA: <C-i> es indistinguible de <Tab> en la terminal. Tab en modo normal
--- también dispara "ventana arriba". En insert/command/visual mode no se toca.
--- TODO: probablemente cambiar esto después, queremos combinaciones tipo Ctrl+k+...
--- como en VSCode en lugar de ocupar Ctrl para navegar ventanas.
+-- Ctrl+j/k/l/i for window navigation (ijkl layout: j=left, i=up, k=down, l=right).
+-- NOTE: <C-i> is indistinguishable from <Tab> in the terminal. Tab in normal mode
+-- also fires "window up". Insert/command/visual mode is unaffected.
+-- TODO: maybe change this later, want Ctrl+k+... combos like VSCode
+-- instead of using Ctrl for window navigation.
 vim.keymap.del("n", "<C-h>")
 vim.keymap.set("n", "<C-j>", "<C-w>h", { desc = "Go to Left Window" })
 vim.keymap.set("n", "<C-i>", "<C-w>k", { desc = "Go to Upper Window" })
 vim.keymap.set("n", "<C-k>", "<C-w>j", { desc = "Go to Lower Window" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window" })
 
--- ijkl (minúsculas) = flechas. En modo visual salen de la selección y solo mueven.
+-- ijkl (lowercase) = arrow keys. In visual mode they exit selection and just move.
 vim.keymap.set("n", "i", "k", { desc = "Move up", remap = false })
 vim.keymap.set("x", "i", "<Esc>k", { desc = "Exit visual and move up" })
 vim.keymap.set("n", "j", "h", { desc = "Move left", remap = false })
@@ -22,13 +22,13 @@ vim.keymap.set("n", "k", "j", { desc = "Move down", remap = false })
 vim.keymap.set("x", "k", "<Esc>j", { desc = "Exit visual and move down" })
 vim.keymap.set("x", "l", "<Esc>l", { desc = "Exit visual and move right" })
 
--- <A-i> / <A-k> = mover línea(s) arriba/abajo (silent para evitar mensajes)
+-- <A-i> / <A-k> = move line(s) up/down (silent to avoid messages)
 vim.keymap.set("n", "<A-i>", "<cmd>silent move .-2<cr>==", { desc = "Move line up" })
 vim.keymap.set("n", "<A-k>", "<cmd>silent move .+1<cr>==", { desc = "Move line down" })
 vim.keymap.set("x", "<A-i>", "<cmd>silent '<,'>move '<-2<cr>gv=gv", { desc = "Move lines up" })
 vim.keymap.set("x", "<A-k>", "<cmd>silent '<,'>move '>+1<cr>gv=gv", { desc = "Move lines down" })
 
--- u = enter insert mode (replaces i)
+-- u = enter insert mode (replaces default i)
 vim.keymap.set("n", "u", "i", { desc = "Enter insert mode" })
 
 -- <C-z> = undo, <C-y> = redo
@@ -43,11 +43,11 @@ vim.keymap.set("n", "<C-p>", LazyVim.pick("files"), { desc = "Find Files (Root D
 -- <C-a> = select all
 vim.keymap.set("n", "<C-a>", "ggVG", { desc = "Select all" })
 
--- d en modo visual borra sin guardar en el portapapeles
+-- d in visual mode deletes without yanking
 vim.keymap.set("x", "d", '"_d', { desc = "Delete without yanking" })
 
--- <BS> en modo normal: si la línea está vacía la elimina (dd), si no borra el carácter
--- anterior y entra en insert (Xi)
+-- <BS> in normal mode: if line is empty delete it (dd), otherwise delete previous
+-- character and enter insert mode (Xi)
 vim.keymap.set("n", "<BS>", function()
   if vim.fn.getline(".") == "" then
     vim.cmd("normal! dd")
@@ -57,14 +57,14 @@ vim.keymap.set("n", "<BS>", function()
   end
 end, { desc = "Backspace: delete char and insert, or delete empty line" })
 
--- cc = yank en modo visual
+-- cc = yank in visual mode
 vim.keymap.set("x", "cc", "y", { desc = "Yank" })
--- ci = yank + mover arriba (recursivo)
+-- ci = yank + move up (recursive)
 vim.keymap.set("x", "ci", "ygvi", { desc = "Yank and move up" })
--- ck = yank + mover abajo (recursivo)
+-- ck = yank + move down (recursive)
 vim.keymap.set("x", "ck", "ygvk", { desc = "Yank and move down" })
 
--- <CR> = nueva línea abajo sin entrar en insert
+-- <CR> = new line below without entering insert
 vim.keymap.set("n", "<CR>", "o<Esc>", { desc = "New line below" })
 
 vim.keymap.set("n", "sl", "V", { desc = "Select line" })
